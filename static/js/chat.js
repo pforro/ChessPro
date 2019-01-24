7208ffc95058677781e62f40e2cf2512e1829e86
peterforro <peterforro@gmail.com>
2019-01-24 11:20:56 +0100

    chat sends message on pressing enter

--- static/js/chat.js
+++ static/js/chat.js
@@ -3,14 +3,20 @@ import {webSocket} from "./websocket.js";
 
 export let chat = {
 
-    initSubmitChat : function() {
-        let submitBtn = document.querySelector('#submit-btn');
-        submitBtn.addEventListener('click',function(){
-            let input = document.querySelector('#chat-input');
-            let usrname = document.querySelector('#chessboard').dataset.nickname;
-            webSocket.sendChat(input.value,usrname);
+    sendChatMessage : function() {
+        let input = document.querySelector('#chat-input');
+        let usrname = document.querySelector('#chessboard').dataset.nickname;
+        if(input.value) {
+            webSocket.sendChat(input.value, usrname);
             input.value = "";
-        })
+        }
+    },
+
+    initSubmitChat : function() {
+        document.querySelector('#submit-btn').addEventListener('click',chat.sendChatMessage);
+        document.querySelector('body').addEventListener('keydown',function(event){
+            if (event.key === 'Enter') chat.sendChatMessage();
+        });
     }
 
 
